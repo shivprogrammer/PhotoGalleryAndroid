2d15dfa86f6f615f2be153f8090e941285cb4cb2
shivprogrammer <shiv.unadkat@gmail.com>
2021-11-06 23:46:51 -0700

    Successfully starting background thread on fragment creation and destroying background thread on fragment destroy

--- PhotoGallery/app/src/main/java/com/shivvy/photogallery/FlickrFetcher.kt
+++ PhotoGallery/app/src/main/java/com/shivvy/photogallery/FlickrFetcher.kt
@@ -1,11 +1,15 @@
 package com.shivvy.photogallery
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.shivvy.photogallery.api.FlickrApi
 import com.shivvy.photogallery.api.FlickrResponse
 import com.shivvy.photogallery.api.PhotoResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -48,5 +52,13 @@ class FlickrFetcher {
         })
 
         return responseLiveData
+    }
+
+    @WorkerThread
+    fun fetchPhoto(url: String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
     }
 }
--- PhotoGallery/app/src/main/java/com/shivvy/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/shivvy/photogallery/PhotoGalleryFragment.kt
@@ -19,10 +19,14 @@ private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoGalleryViewModel : PhotoGalleryViewModel
     private lateinit var photoRecyclerView : RecyclerView
+    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        retainInstance = true
         photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+        thumbnailDownloader = ThumbnailDownloader()
+        lifecycle.addObserver(thumbnailDownloader)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View {
@@ -39,6 +43,13 @@ class PhotoGalleryFragment : Fragment() {
                 Observer { galleryItems ->
                     photoRecyclerView.adapter = PhotoAdapter(galleryItems)
                 }
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycle.removeObserver(
+                thumbnailDownloader
         )
     }
 
